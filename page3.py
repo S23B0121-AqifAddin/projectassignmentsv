83c24f68964d8f20bccebd463d0063562942e047
Khadijah <s22b0008@siswa.umk.edu.my>
2025-12-27 14:16:31 +0800

    Update page3.py

--- page3.py
+++ page3.py
@@ -1,1 +1,6 @@
-
+# Load your data
+try:
+    df2 = pd.read_csv('https://raw.githubusercontent.com/S23B0121-AqifAddin/projectassignmentsv/refs/heads/main/processed_financial_capability_data.csv', encoding='utf-8')
+except UnicodeDecodeError:
+    df2 = pd.read_csv('https://raw.githubusercontent.com/S23B0121-AqifAddin/projectassignmentsv/refs/heads/main/processed_financial_capability_data.csv', encoding='latin-1')
+df2
