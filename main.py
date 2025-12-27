6563db6dbe61956580ff91ef0df9ddaba81c5694
aisyah2521 <s22a0046@siswa.umk.edu.my>
2025-12-27 15:03:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Financial Behaviour among University Students")
 
 # Define each page
-page_1 = st.Page("page1.py", title="Page 1")
+page_1 = st.Page("page1.py", title="Budgeting & Spending Behavior")
 page_2 = st.Page("page2.py", title="Page 2")
 page_3 = st.Page("page3.py", title="Consumer Rights")
 page_4 = st.Page("page4.py", title="Page 4")
