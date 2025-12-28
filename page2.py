eeffecc9cac5b72c271640f70ae16dd6f0353393
S23B0121-AqifAddin <s23b0121@siswa.umk.edu.my>
2025-12-29 03:52:20 +0800

    Update page2.py

--- page2.py
+++ page2.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 # --- Corrected Imports ---
 import plotly.graph_objects as go # Keep this if you need go, though px handles everything here
@@ -29,31 +28,3 @@ try:
 except UnicodeDecodeError:
     df2 = pd.read_csv('https://raw.githubusercontent.com/S23B0121-AqifAddin/projectassignmentsv/refs/heads/main/processed_financial_capability_data.csv', encoding='latin-1')
 df2
-
-# Title Bar Chart
-st.title("Financial Decision-Making")
-
-# Load data 
-url = "https://raw.githubusercontent.com/S23B0121-AqifAddin/projectassignmentsv/main/processed_financial_capability_data.csv"
-data = pd.read_csv(url)
-
-# Check column name
-st.write("Dataset Preview:")
-st.dataframe(data.head())
-
-# Plot
-fig, ax = plt.subplots(figsize=(10, 6))
-
-sns.countplot(
-    y='Monthly_Income',
-    data=data,
-    order=data['Monthly_Income'].value_counts().index,
-    palette='viridis',
-    ax=ax
-)
-
-ax.set_title('Distribution of Monthly Income Range')
-ax.set_xlabel('Count')
-ax.set_ylabel('Monthly Income Range')
-
-st.pyplot(fig)
