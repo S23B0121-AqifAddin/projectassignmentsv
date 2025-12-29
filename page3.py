fe94e9afd69d4b1f9f55fd40cb5cb03a6c67c245
Khadijah <s22b0008@siswa.umk.edu.my>
2025-12-29 15:20:36 +0800

    Update page3.py

--- page3.py
+++ page3.py
@@ -29,7 +29,7 @@ try:
     df = pd.read_csv('https://raw.githubusercontent.com/S23B0121-AqifAddin/projectassignmentsv/refs/heads/main/processed_financial_capability_data.csv', encoding='utf-8')
 except UnicodeDecodeError:
     df = pd.read_csv('https://raw.githubusercontent.com/S23B0121-AqifAddin/projectassignmentsv/refs/heads/main/processed_financial_capability_data.csv', encoding='latin-1')
-df2
+df
 
 
 # DISPLAY VISUALIZATION
