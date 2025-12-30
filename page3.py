1013c3252447d08429dd2c98da88c3365f0cf814
Khadijah <s22b0008@siswa.umk.edu.my>
2025-12-30 17:26:34 +0800

    Update page3.py

--- page3.py
+++ page3.py
@@ -99,7 +99,7 @@ st.pyplot(fig)
 
 #Pie Chart
 st.subheader("Complaint Behavior for Unsuitable Products")
-st.write("This pie chart shows the proportion of consumers who lodged complaints regarding unsuitable products.")
+st.write("This pie chart shows the proportion of consumers who submit complaints regarding unsuitable products.")
 # Calculate the value counts for 'Complaint_for_Unsuitable_Product'
 complaint_counts = df['Complaint_for_Unsuitable_Product'].value_counts()
 # Create the figure
