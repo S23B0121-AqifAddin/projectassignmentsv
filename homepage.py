f5cde4f24597ec6d4289c42ba546d0f3bd979a9c
Khadijah <s22b0008@siswa.umk.edu.my>
2026-01-03 03:09:52 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -8,7 +8,8 @@ st.markdown(
         background-image: url("https://images.unsplash.com/photo-1526304640581-d334cdbbf45e");
         background-size: cover;
         background-position: center;
-        color: black;
+        color: white;  /* Text fill color */
+        -webkit-text-stroke: 2px black; /* Black outline thickness */
         padding: 30px;
         border-radius: 12px;
         font-size: 2.5rem;
