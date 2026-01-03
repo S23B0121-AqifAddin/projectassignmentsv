e80bbf89e52d0a6a7a50a09923bba2709a28d8ad
Khadijah <s22b0008@siswa.umk.edu.my>
2026-01-04 07:53:47 +0800

    Update page3.py

--- page3.py
+++ page3.py
@@ -69,6 +69,9 @@ try:
     df = pd.read_csv('https://raw.githubusercontent.com/S23B0121-AqifAddin/projectassignmentsv/refs/heads/main/Datasets/(Khadijah)%20processed_financial_capability_data.csv', encoding='utf-8')
 except UnicodeDecodeError:
     df = pd.read_csv('https://raw.githubusercontent.com/S23B0121-AqifAddin/projectassignmentsv/refs/heads/main/Datasets/(Khadijah)%20processed_financial_capability_data.csv', encoding='latin-1')
+
+# Display row × column info (like Colab) in smaller, lighter text
+st.markdown(f"<div style='font-size:12px; color:gray; margin-bottom:4px;'>Rows: {df.shape[0]} × Columns: {df.shape[1]}</div>", unsafe_allow_html=True)
 df
 
 # =========================
