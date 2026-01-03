3f1294b19b7d5b7488886a7d9844d77dd0ec7422
Khadijah <s22b0008@siswa.umk.edu.my>
2026-01-03 14:53:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ st.set_page_config(
 )
 
 # Define each page
-Home_page = st.Page("homepage.py", title="👥 Group Overview")
+Home_page = st.Page("homepage.py", title="👥 Group Overview", icon=":material/groups:")
 
 page_1 = st.Page("page1.py", title="(Aisyah) Budgeting & Spending Behavior")
 page_2 = st.Page("page2.py", title="(Aqif) Financial Decision-Making")
-page_3 = st.Page("page3.py", title="(Khadijah) Consumer Rights", icon=":material/receipt_long:")
+page_3 = st.Page("page3.py", title="(Khadijah) Consumer Rights 📜")
 page_4 = st.Page("page4.py", title="(Kisantini) Consumer Awareness & Information Seeking")
 
 # Create navigation menu
