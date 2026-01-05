c4bf9e6a676d0b63aa420229e180a856223d360c
Khadijah <s22b0008@siswa.umk.edu.my>
2026-01-05 21:22:11 +0800

    Update page3.py

--- page3.py
+++ page3.py
@@ -578,3 +578,24 @@ elif viz_option == "Financial Knowledge vs Complaint (Faceted Bar)":
     )
 
     st.plotly_chart(fig, use_container_width=True)
+
+
+st.write("""
+Click a section below to view each member’s contribution.
+""")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("📊 Aisyah – Budgeting & Spending Behaviour", use_container_width=True):
+        st.switch_page("page1.py")
+
+    if st.button("🧠 Aqif – Financial Decision-Making", use_container_width=True):
+        st.switch_page("page2.py")
+
+with col2:
+    if st.button("🧾 Khadijah – Consumer Rights", use_container_width=True):
+        st.switch_page("page3.py")
+
+    if st.button("🔍 Kisantini – Consumer Awareness", use_container_width=True):
+        st.switch_page("page4.py")
