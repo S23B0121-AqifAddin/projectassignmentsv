265aa53799965d9502dc40300dc6120bf92bb851
Khadijah <s22b0008@siswa.umk.edu.my>
2026-01-05 21:55:27 +0800

    Update page2.py

--- page2.py
+++ page2.py
@@ -264,11 +264,11 @@ Click a section below to view another page.
 col1, col2 = st.columns(2)
 
 with col1:
-    if st.button("📊 Aisyah – Budgeting & Spending Behaviour", use_container_width=True):
-        st.switch_page("page1.py")
-
     if st.button("🧾 Khadijah – Consumer Rights", use_container_width=True):
         st.switch_page("page3.py")
+        
+    if st.button("📊 Aisyah – Budgeting & Spending Behaviour", use_container_width=True):
+        st.switch_page("page1.py")
 
 with col2:
     if st.button("🔍 Kisantini – Consumer Awareness", use_container_width=True):
